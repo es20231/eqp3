db1c68d60e8b872e8326d71466925e6a5412bb37
Luis Eduardo Alencar <83970615+eduardoadf21@users.noreply.github.com>
2023-06-09 12:01:52 -0300

    Add files via upload
    local upload, user database, authentication

---
+++ djavu/controllers/auth.py
@@ -0,0 +1,66 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from djavu.db import get_db
+from djavu.repository import userRepository
+
+bp = Blueprint('auth', __name__, url_prefix='/')
+
+repo = userRepository()
+
+@bp.route('/', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        error = None
+
+        user = repo.search_user(username)
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+            return redirect(url_for('blog.index'))
+
+        flash(error)
+
+    return render_template('login.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('auth.login'))
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
+
+
