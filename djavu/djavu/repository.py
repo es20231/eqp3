026b342e0a576f16e9a0f0341f8ecc8d8263f4cc
Luis Eduardo Alencar <83970615+eduardoadf21@users.noreply.github.com>
2023-06-09 11:56:34 -0300

    Add files via upload
    local upload, user database, authentication

---
+++ djavu/djavu/repository.py
@@ -0,0 +1,28 @@
+from djavu.db import get_db
+from werkzeug.security import generate_password_hash
+
+class userRepository:
+    def list_users(self):
+        db = get_db()
+        return db.execute(
+            'SELECT * FROM user'
+        ).fetchall()
+
+    def insert_user(self, username, fullname, email, password):
+        db = get_db()
+        try:
+            db.execute(
+                "INSERT INTO user (username, fullname, email, password) VALUES (?, ?, ?, ?)",
+                (username, fullname,
+                 email, generate_password_hash(password)),
+            )
+            db.commit()
+        except db.IntegrityError:
+            error = f"User {username} is already registered."
+
+    def search_user(self, username):
+        db = get_db()
+        return db.execute(
+            'SELECT * FROM user WHERE username = ?', (username,)
+        ).fetchone()
+
