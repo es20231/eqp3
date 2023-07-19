e75c62337e5f8235fa7b34b3526d2aad00a5d31f
Marcelo Eduardo Rufino <fmarcelo3duardo@gmail.com>
2023-07-18 21:25:01 -0300

    Remove button ListImage, StrictMode. Config styled

--- client/src/Components/ImportListImage/index.js
+++ client/src/Components/ImportListImage/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { Button, Row, Col } from "react-bootstrap";
 import { useApi } from "../../hooks/UseApi";
+import { useEffect } from "react";
 import './styles.scss'
 import FotosGaleria from "../FotosGaleria";
 
@@ -10,25 +11,33 @@ function ImportListImage() {
     const api = useApi();
     const [imagemDownload, setImagemDownload] = useState([]);
 
-    async function ImportImagensApi() {
-        setImagemDownload([]);
-        const images = await api.importListImage();
-        console.log(images.data);
+    useEffect(() => {
+        //Runs only on the first render
+         const ImportImagensApi = async () => {
+            setImagemDownload([]);
+            const images = await api.importListImage();
+            console.log(images.data);
 
-        images.data.map(async (dataName) => {
-            const importImages = await api.importImage(dataName);
-            setImagemDownload((prev) => [...prev, importImages]);
-        });
+            images.data.map(async (dataName) => {
+                const importImages = await api.importImage(dataName);
+                setImagemDownload((prev) => [...prev, importImages]);
+            });
 
-        console.log("teste de armazenamento");
-        console.log(imagemDownload);
-    }
+            console.log("teste de armazenamento");
+            console.log(imagemDownload);
+        }
+
+        ImportImagensApi();
+
+    }, []);
+
+
 
     return (
         <>
-            <Button type="button" onClick={ImportImagensApi}>
+            {/* <Button type="button" onClick={ImportImagensApi}>
                 baixar imagens do usuário
-            </Button>
+            </Button> */}
             <Row>
                 {imagemDownload.map((urlImg) => (
 
