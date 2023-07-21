3e299db707d80ef0efe4360063cabd04cdcd0c74
LUCIANOGFORTES02 <lucianogfortes@gmail.com>
2023-07-21 18:32:43 -0300

    teste Cadastro

--- client/src/Components/testes/Login.test.js
+++ client/src/Components/testes/Login.test.js
@@ -8,76 +8,38 @@ import { UserProvider } from '../../Contexts/Auth/AuthContext';
 import Home from '../../Routes/Home';
 import { UserContext } from "../../Contexts/Auth/AuthContext";
 import { MemoryRouter } from 'react-router-dom';
-
-
-// const mockNavigate = jest.fn();//Retorna uma função fake
-//Toda vez que o componete for fazer um import do react - router o jest vai substituir por essa declaração 
-// jest.mock('react-router', () => ({
-//   ...jest.requireActual('react-router'),
-//   useNavigate: () => mockNavigate
-// }))
-
-
-
-// Mock do contexto de usuário
-// jest.mock('../../Contexts/Auth/AuthContext', () => ({
-//   UserContext: {
-//     Consumer: ({ children }) => children({ login: jest.fn() }), // Simula a função de login do contexto
-//   },
-// }));
-
-
-
-// Mock do contexto de autenticação (UserContext)
-// const mockUserContextValue = {
-//   user: {
-//     token: "mock-token",
-//   },
-// };
-
-// jest.mock("../../Contexts/Auth/AuthContext", () => ({
-//   UserContext: {
-//     Consumer: (props) => props.children(mockUserContextValue),
-//   },
-// }));
-
-// Mock do contexto UserContext
-
-/*
-const renderComponente= ()=>{
-    render (
-        <BrowserRouter>
-            <UserProvider>
-                <Home/>
-            </UserProvider>
-        </BrowserRouter>
-    )
-
-    return { }
-}
-
-*/
-// Mock do contexto UserContext
-const mockUserContext = {
-  login: jest.fn(), // Vamos criar um mock para a função login
-};
-
-// Mock do react-router-dom useNavigate
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useNavigate: () => jest.fn(), // Vamos criar um mock para useNavigate
-}));
+import { useApi } from '../../hooks/UseApi';
+import { toast } from 'react-toastify';
 
 // Mock do react-toastify toast
 jest.mock('react-toastify', () => ({
   toast: {
     success: jest.fn(),
-    warning: jest.fn(),
+    error: jest.fn(),
   },
 }));
 
+// Criar um mock para a função login da API
+jest.mock('../../hooks/UseApi', () => ({
+  useApi: jest.fn(),
+}));
 
-describe("Login do usuário", () => {
+// Função de utilidade para retornar uma promise resolvida com o valor especificado
+const resolvedPromise = (value) => Promise.resolve(value);
+
+describe('Home Component', () => {
+  // Mock do contexto do usuário
+  const mockUserContext = {
+    login: jest.fn(),
+  };
+
+  beforeEach(() => {
+    // Limpar mocks e dados antes de cada teste
+    jest.clearAllMocks();
+    useApi.mockReturnValue({
+      login: jest.fn(),
+    });
+  });
 
   test("testando o botão criar conta ", () => {
     //Função para renderizar o componente
@@ -95,14 +57,19 @@ describe("Login do usuário", () => {
 
     expect(screen.getByText("Criar Conta")).toBeInTheDocument();
 
-  })
-  test('Testando a submissão do login', () => {
+  })  
+
+  test('testando a submissão do login', async () => {
+    // Simular a função login da API retornando uma promise resolvida
+    useApi.mockReturnValue({
+      login: jest.fn().mockReturnValue(resolvedPromise({ status: 200 })),
+    });
+
     render(
-        
       <MemoryRouter>
-      <UserContext.Provider value={mockUserContext}>
-        <Home />
-      </UserContext.Provider>
+        <UserContext.Provider value={mockUserContext}>
+          <Home />
+        </UserContext.Provider>
       </MemoryRouter>
     );
 
@@ -113,104 +80,48 @@ describe("Login do usuário", () => {
     // Simular o clique no botão de Entrar
     fireEvent.click(screen.getByRole('button', { name: 'Entrar' }));
 
-    // Verificar se a função login do contexto UserContext foi chamada corretamente
-    expect(mockUserContext.login).toHaveBeenCalledWith('john.doe', 'password123');
+    // Aguardar a execução das tarefas assíncronas (no caso, a função login da API)
+    await Promise.resolve();
+
+    // Verificar se a função login da API foi chamada corretamente
+    expect(useApi().login).toHaveBeenCalledWith('john.doe', 'password123');
+    // Verificar se o toast de sucesso foi exibido
+    expect(toast.success).toHaveBeenCalledWith('login ok');
   });
 
+ 
 
-  
+  // test('displays error toast if login fails', async () => {
+  //   // Simular a função login da API retornando uma promise rejeitada
+  //   useApi.mockReturnValue({
+  //     login: jest.fn().mockRejectedValue({ status: 401 }),
+  //   });
 
-/*
-  test("Testando botão de Entrar", () => {
-    render(
-        
-      <MemoryRouter>
-      <UserContext.Provider value={mockUserContext}>
-        <Home />
-      </UserContext.Provider>
-      </MemoryRouter>
-    );;
+  //   render(
+  //     <MemoryRouter>
+  //       <UserContext.Provider value={mockUserContext}>
+  //         <Home />
+  //       </UserContext.Provider>
+  //     </MemoryRouter>
+  //   );
 
-    //Salvar o elemento em uma constante
-    const btnEntrar = screen.getByText("Entrar");
-    //Função utilitaria que dispara um evento
-    fireEvent.click(btnEntrar)
-    //Espero que a função navigate tenha sido chamada com o parâmetro correto
-    expect(mockNavigate).toHaveBeenCalledWith('/Private');
+  //   // Simular a digitação de um nome de usuário e senha inválidos
+  //   fireEvent.change(screen.getByLabelText('Name User'), { target: { value: 'invalid_user' } });
+  //   fireEvent.change(screen.getByLabelText('Password'), { target: { value: 'invalid_password' } });
 
-  })
+  //   // Simular o clique no botão de Entrar
+  //   fireEvent.click(screen.getByRole('button', { name: 'Entrar' }));
 
+  //   // Aguardar a execução das tarefas assíncronas (no caso, a função login da API)
+  //   await Promise.resolve();
 
-
-  // Teste de interação do usuário
-  test('chama a função isLogged quando o formulário é enviado com dados válidos', () => {
-
-    render(
-        
-      <MemoryRouter>
-      <UserContext.Provider value={mockUserContext}>
-        <Home />
-      </UserContext.Provider>
-      </MemoryRouter>
-    );
-
-    const mockIsLogged = jest.fn(); // Simula a função isLogged
-
-    const nameInput = screen.getByLabelText(/Name User/i);
-    const passwordInput = screen.getByLabelText(/Password/i);
-    const submitButton = screen.getByText(/Entrar/i);
-
-    // Preenche os campos do formulário
-    fireEvent.change(nameInput, { target: { value: 'testUser' } });
-    fireEvent.change(passwordInput, { target: { value: 'testPassword' } });
-
-    // Submete o formulário
-    fireEvent.click(submitButton);
-
-    // Verifica se a função isLogged foi chamada corretamente
-    expect(mockIsLogged).toHaveBeenCalledTimes(1);
-  });
-
-
-  // Teste de exibição de mensagem de aviso
-  test('exibe toast de aviso quando o login falha', () => {
-
-    render(
-        
-      <MemoryRouter>
-      <UserContext.Provider value={mockUserContext}>
-        <Home />
-      </UserContext.Provider>
-      </MemoryRouter>
-    );
-
-
-    const mockIsLogged = jest.fn().mockReturnValue(false); // Simula a função isLogged retornando falso
-
-    const nameInput = screen.getByLabelText(/Name User/i);
-    const passwordInput = screen.getByLabelText(/Password/i);
-    const submitButton = screen.getByText(/Entrar/i);
-
-    // Preenche os campos do formulário
-    fireEvent.change(nameInput, { target: { value: 'testUser' } });
-    fireEvent.change(passwordInput, { target: { value: 'testPassword' } });
-
-    // Submete o formulário
-    fireEvent.click(submitButton);
-
-    // Verifica se a mensagem de aviso é exibida corretamente
-    const warningToast = screen.getByText(/necessário fazer um login/i);
-    expect(warningToast).toBeInTheDocument();
-
-
-  });
+  //   // Verificar se a função login da API foi chamada corretamente
+  //   expect(useApi().login).toHaveBeenCalledWith('invalid_user', 'invalid_password');
+  //   // Verificar se o toast de erro foi exibido
+  //   expect(toast.error).toHaveBeenCalledWith('dados errados');
+  // });
 
 
 
-
-
-*/
-
-})
-
+});
 
