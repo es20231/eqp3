74381194217f3903cc4f2304d53708d3573962a1
eduardoadf21 <eduardo.adf8@protonmail.com>
2023-08-16 16:33:06 -0300

    user/<username>

--- api/api/controllers/profile.py
+++ api/api/controllers/profile.py
@@ -30,6 +30,16 @@ def userdata():
             data[atributo] = raw_data[atributo]
     
     return jsonify(data)
+
+@bp.route('/user/<username>', methods=['GET',])
+def userData(username):
+    raw_data = Users.search_username(username)
+    data = {}
+    for atributo in raw_data.keys():
+        if atributo != "password" and atributo != "role":
+            data[atributo] = raw_data[atributo]
+    
+    return jsonify(data)
     
 @bp.route('/change_username', methods=['POST',])
 @login_required
